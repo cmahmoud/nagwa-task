4967a1f206164f37a8dd6eb439dfde411a509282
officialzoka <golldenhouse@gmail.com>
2022-12-12 13:51:34 +0200

    space added

--- server/src/routes/task.ts
+++ server/src/routes/task.ts
@@ -11,6 +11,7 @@ const router = Router();
 router.get("/words", async (req: Request, res: Response) => {
     // get random 10 words with including all pos
     const words = getRandomWords(data.wordList);
+
     // return words result
     res.status(200).json(words);
 });
@@ -21,16 +22,20 @@ router.get("/words", async (req: Request, res: Response) => {
 router.post("/rank", async (req: Request, res: Response) => {
     // get score from body
     const { score } = req.body;
+
     // filter numbers that smaller than score
     const filteredScoreList: number[] = data.scoresList.filter(
         (item) => item < Number(score)
     );
+
     // calculate rank
     // divide length of filtered array over length of score list array
     let rank: number =
         (filteredScoreList.length / data.scoresList.length) * 100;
+
     // convert to fixed if float or keep value if number
     rank = Number.isInteger(rank) ? rank : Number(rank.toFixed(2));
+
     // return rank result
     res.status(200).json({ rank });
 });
